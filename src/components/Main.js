c3fadd3c3419075ed166dd771cefaf871d85f0ec
Hemslo Wang <hemslo.wang@gmail.com>
2015-12-24 23:55:08 +0800

    Add basic web

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,21 +2,81 @@ require('normalize.css');
 require('styles/App.css');
 
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
+import keymakerToolbox from 'keymaker-toolbox';
 
 class AppComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      key: '',
+      password: '',
+      salt: '',
+      iterations: props.iterations,
+      keylen: props.keylen,
+      lower: props.lower,
+      upper: props.upper,
+      number: props.number,
+      special: props.special
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      key: keymakerToolbox.makeKey(this.state.password,
+                                   this.state.salt,
+                                   this.state.iterations,
+                                   this.state.keylen,
+                                   this.lower,
+                                   this.upper,
+                                   this.number,
+                                   this.special)
+    });
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({password: e.target.value});
+  }
+
+  handleSaltChange = (e) => {
+    this.setState({salt: e.target.value});
+  }
+
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <form className="keymakerForm" onSubmit={this.handleSubmit}>
+          <input
+            type="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
+          <input
+            type="password"
+            placeholder="Salt"
+            value={this.state.salt}
+            onChange={this.handleSaltChange}
+          />
+          <input type="submit" value="Post" />
+          <input
+            type="text"
+            value={this.state.key}
+            disabled="true"
+          />
+        </form>
       </div>
     );
   }
 }
 
 AppComponent.defaultProps = {
+  iterations: 100000,
+  keylen: 32,
+  lower: true,
+  upper: true,
+  number: true,
+  special: true
 };
 
 export default AppComponent;
